8c0e27b1491762c5d22d50b0dcb144fd70036948
Nikhil Nair <niks.nair@gmail.com>
2018-07-11 14:01:15 +0530

    updated with nodemailer

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
-var express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var nodemailer = require("nodemailer");
 
+var port = 3000;
+
+//setting up the express and bodyparser apps
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+//ejs template engine, assets for static resources
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
-/*
----- rubin's explanation ----
-first argument passed in the use function is basically saying that anyone
-who tries to access files inside /assets should be served with the files
-inside the "stuff" dir, which is the second parameter of the use function.
-if you look at the link tag inside your html,
-it says "href="/assets/style.css"",
-here in this case you are trying to access the /assets folder, so then
-you are served with the files inside the stuff dir(which is indicated on
-the second param of the use function).
-*/
-/*
-middleware, routed, without express
-app.use('/assets', function(req, res, next){
-	console.log(req.url);
-	next();
-});
-*/
-var dTime = new Date();
-console.log('[server-refresh] logged: ' + dTime);
+//connect to dlab
+mongoose.connect('mongodb://demosender:demohpc1@ds131971.mlab.com:31971/demo-hpc', { useNewUrlParser: true });
 
-/*
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
-});
-*/
+//get requests for all pages
 app.get('/', function(req, res){
 	res.render('index');
 });
@@ -53,11 +39,56 @@ app.get('/faq', function(req, res){
 app.get('/contact', function(req, res){
 	res.render('contact');
 });
-/*
-app.get('/page/:object', function(req, res){
-	var obj1 = {prop1: val1/string, prop2: val2/string};
-	res.render('index', {obj: req.params.object, obj1: obj1});
-});
-*/
 
-app.listen(3000);
+app.get('/privpol', function(req, res){
+	res.render('privpol');
+});
+app.get('/formsuccess', function(req, res){
+	res.render('formsuccess');
+});
+
+//database schema
+var demoschema = new mongoose.Schema({
+  name: String,
+  industry: String,
+  email: String,
+  mobile: String
+});
+//creating model type
+var demo = mongoose.model('demo', demoschema);
+//transport service for nodemailer
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'demo.hydphcity@gmail.com',
+    pass: 'demohpc1'
+  }
+});
+
+//form content parsing, post method
+app.post("/sendmail", urlencodedParser, function(req, res){
+ //get info from view and pass to mongodb
+ var newdemo = demo(req.body).save(function(err, data){
+   if (err) throw err;
+ });
+ //email details
+ var mailOptions = {
+   from: 'demo.hydphcity@gmail.com', // sender address
+   to: req.body.email, // list of receivers
+   subject: 'Thanks for subscribing', // Subject line
+   html: '<p>Random html text</p>'// plain text body
+ };
+ //sending the email
+ transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+      console.log(err)
+    else
+      console.log(info);
+ });
+ res.render(__dirname + "/views/formsuccess");
+});
+
+var dTime = new Date();
+app.listen(port, function(){
+ console.log('[server-refresh] logged: ' + dTime);
+});
